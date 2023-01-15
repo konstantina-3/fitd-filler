0c574893766d8c4f72afa1fd78474ac74818ed6b
Konstantina Giannakopoulou <konstantina.gian@gmail.com>
2023-01-15 21:11:25 +0200

    Add bracket input

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
+import { useState } from 'react';
 import Round from './Round.js';
 import './App.css';
 
-let initial_players = [
-  "Player 1",
-  "Player 2",
-  "Player 3",
-  "Player 4",
-  "Player 5",
-  "Player 6",
-  "Player 7",
-  "Player 8",
-  "Player 11",
-  "Player 12",
-  "Player 13",
-  "Player 14",
-  "Player 15",
-  "Player 16",
-  "Player 17",
-  "Player 18",
-];
-
 function App() {
+  const [players, setPlayers] = useState(null);
+
+  function isPowerOf2(number) {
+    return number && !(number & (number - 1));
+  }
+
+  function handleTextAreaChange(e) {
+    let input_players = e.target.value
+      .split('\n')
+      .filter(item => item)
+      .map(item => item.trim().replace(/\s\s+/g, ' '));
+
+    if (isPowerOf2(input_players.length)) {
+      setPlayers(input_players);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Fill in the Draw!
         </p>
+        <div className='players-input'>
+            <textarea
+              onChange={handleTextAreaChange}
+              placeholder="Enter the first round matches by listing all players on individual lines."
+              name="players-input" id="players-input" cols="80" rows="20">
+            </textarea>
+        </div>
         <div className='rounds'>
-          <Round players={initial_players} />
+          {players && <Round players={players} key={players}/>}
         </div>
       </header>
     </div>
--- src/Round.js
+++ src/Round.js
@@ -8,7 +8,7 @@ function Round({ players }) {
     if (!e.target.innerText) return;
 
     const insertAt = Math.floor(e.target.dataset.key/2);
-    
+
     const nextWinners = [
       ...winners.slice(0, insertAt),
       e.target.innerText,
@@ -16,26 +16,27 @@ function Round({ players }) {
       ...winners.slice(insertAt + 1)
     ];
 
-    console.log(nextWinners)
-
     setWinners(nextWinners);
   }
 
-  for (let i = 0; i < players.length; i += 2) {
-    if (winners[i/2] && winners[i/2] !== players[i] && winners[i/2] !== players[i+1]) {
-        // should a match winner change we want their old winner's potential further round wins to be emptied
+  if (players) {
+    // should a match winner change we want their old winner's potential further round wins to be emptied
+    for (let i = 0; i < players.length; i += 2) {
+      if (winners[i/2] && winners[i/2] !== players[i] && winners[i/2] !== players[i+1]) {
         const nextWinners = [
             ...winners.slice(0, i/2),
             undefined,
             ...winners.slice(i/2 + 1)
-          ];
+        ];
+
         setWinners(nextWinners);
-    }    
+      }
+    }
   }
 
   return (
     <>
-        <ul className='round'>{players.map((player, index) =>
+        <ul className='round'>{players && players.map((player, index) =>
             <li className={`player ${!player ? 'disabled' : ''}`}
                 key={index}
                 data-key={index}
@@ -43,7 +44,7 @@ function Round({ players }) {
                 <span>{player}</span>
             </li>
         )}</ul>
-        {players.length > 1 && <Round players={winners} />}
+        {players && players.length > 1 && <Round players={winners} />}
     </>
   );
 }
