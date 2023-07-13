0832a1e45166c5e2466e9a70cc96714941489f5f
Konstantina Giannakopoulou <konstantina.gian@gmail.com>
2023-07-14 00:33:20 +0300

    Add copy button to get the winners of each round in TF FITD format

--- src/App.js
+++ src/App.js
@@ -18,21 +18,62 @@ function App() {
     if (isPowerOf2(input_players.length)) {
       setPlayers(input_players);
     }
+    console.log(input_players)
+  }
+
+  async function copyResult() {
+    let text = "";
+
+    const rounds = document.querySelectorAll('.round-container');
+    rounds.forEach(el => {
+      const players = [...el.querySelectorAll('.player')]
+                  .map(player => player.innerHTML);
+
+      const round = el.querySelector('.round-info').innerHTML;
+
+      if (round === "Round 1") return; // we want the winners of R1 and so on
+
+      switch (players.length) {
+        case 1:
+          text += 'F\n';
+          break;
+        case 2:
+          text += 'SF\n';
+          break;
+        case 4:
+          text += 'QF\n';
+          break;
+        default:
+          text += "R" + (Number(round.slice(-1)) - 1) + '\n';
+      }
+
+      text += players.join('\n') + '\n';
+    });
+
+    try {
+      await navigator.clipboard.writeText(text);
+      // console.log(text);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
   }
 
   return (
     <div className="App">
       <header>
         <h1>Fill in the Draw!</h1>
-        <h2>Creates an interactive bracket of any tournament size (2<sup>n</sup> participants)</h2>
+        <h2>Paste the draw (of any tournament size), fill it in using the interactive bracket and then copy the full result!</h2>
       </header>
       <textarea
         onChange={handleTextAreaChange}
         placeholder="Enter each first round match in a new line with the player names separated by ' vs '. E.g:
 Swiatek vs Zhu
-Sabalenka vs Udvardy"
+Udvardy vs Sabalenka"
         name="players-input" id="players-input" rows="20">
       </textarea>
+      <div className='copy-button'>
+        <button type='button' onClick={copyResult}>Copy Result</button>
+      </div>
       <div className='rounds'>
         {players && <Round players={players} key={players}/>}
       </div>
